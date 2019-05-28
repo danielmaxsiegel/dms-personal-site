876b2c56e017266f5c211d1ee08cd38a9cea563a
mrdanielmax <danielmaxsiegel@gmail.com>
2019-05-28 16:02:26 -0700

    added copyright section and better random class generation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-// Main
+// add random pattern to section titles
+var sectionTitles = document.getElementsByClassName("section-title");
+
+var randomPatternClass = "section-title-style-" + getRandomInt(3)
+
+for (var i = 0; i < sectionTitles.length; ++i) {
+  sectionTitles[i].classList.add(randomPatternClass);
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
