45e549a7a237719a25c76daf4592de317fb4840d
Daniel Max <danielmaxsiegel@gmail.com>
2019-12-02 21:41:26 -0800

    wired in the images and descriptions I have written

--- js/projectModals.js
+++ js/projectModals.js
@@ -51,7 +51,7 @@ function getProjectModal(project, modalKey) {
                   '</div>' +
                   '<div class="divider-custom-line"></div>' +
                 '</div>' +
-                '<img class="img-fluid rounded mb-5" src="' + project.imageUrl + '" alt="' + project.name + '">' +
+                '<img class="img-fluid rounded mb-5" src="img/projects/screenshots/' + project.imageUrl + '" alt="' + project.name + '">' +
                 '<p class="mb-5">' + project.description + '</p>' +
                 '<button class="btn btn-primary" href="#" data-dismiss="modal">' +
                   '<i class="fas fa-times fa-fw"></i>' +
@@ -71,79 +71,79 @@ function getProjects() {
       {
     		"name": "5to9",
     		"subtitle": "Side Project Collective",
-    		"description": "",
+    		"description": "A collective for developers who love passion projects and collaboration. 5to9 is an effort by myself and some fellows from university to create a group where people can work on side projects without necessarily planning on monetizing them. The group has done work in NodeJS, Swift, Java, Python/Jupyter Notebook and more.",
     		"technologies": "teamwork",
-    		"imageUrl": "/img/projects/screenshots/.png"
+    		"imageUrl": ""
     	},
       {
     		"name": "Crowdium",
     		"subtitle": "Can you e-lance a whole business?",
         "description": "",
     		"technologies": "",
-    		"imageUrl": "/img/projects/screenshots/.png"
+    		"imageUrl": ""
     	},
       {
     		"name": "Shelfaudit",
     		"subtitle": "Author diversity analyzer",
         "description": "",
     		"technologies": "",
-    		"imageUrl": "/img/projects/screenshots/.png"
+    		"imageUrl": ""
     	},
       {
     		"name": "Quality Poetry",
     		"subtitle": "Randomly Generated Poetry",
-        "description": "",
+        "description": "I was speaking with a professional photographer friend of mine about her difficulties when it comes to captioning her pictures. We joked about captioning everything with bad poetry, and Quality Poetry was born out of that conversation. Quality Poetry, found at qualitypoetry.com, is a webapp that procedurally generates poems.",
     		"technologies": "",
-    		"imageUrl": "/img/projects/screenshots/.png"
+    		"imageUrl": "qualityPoetry.png"
     	},
       {
     		"name": "LotOLife",
     		"subtitle": "Procedural People Generator",
-        "description": "",
+        "description": "A tool that procedurally generates a fake human being using real-world statistics. Starting with gender and country of origin, the tool attempts to generate personal information, abilities, and more. The name comes from the fact that there’s a “Lot O’ Life”, and that the random selection of traits in life is kind of like playing the Lotto. Find Lotolife here.",
     		"technologies": "",
-    		"imageUrl": "/img/projects/screenshots/.png"
+    		"imageUrl": "lotOLife.png"
     	},
       {
     		"name": "Okay Pill",
     		"subtitle": "A Fashion Brand Website with a Twist",
-        "description": "",
+        "description": "Okay pill is a clothing brand built around mental health awareness. As a friend of the founder, I volunteered to build their website per his designs. The website is built in NodeJS using React.",
     		"technologies": "React",
-    		"imageUrl": "/img/projects/screenshots/.png"
+    		"imageUrl": "okayPill.png"
     	},
       {
     		"name": "Music Visualizer",
     		"subtitle": "Unity Project",
-        "description": "",
+        "description": "A simple audio visualizer built for fun in Unity, with scripts written in C#. The program detects the beat of music and pulses lights accordingly. It was meant to be an early prototype of a more fleshed-out music visualizer.",
     		"technologies": "",
-    		"imageUrl": "/img/projects/screenshots/.png"
+    		"imageUrl": "unityVisualizer.gif"
     	},
       {
     		"name": "HowAmI",
     		"subtitle": "Mood Tracking iOS App",
-        "description": "",
+        "description": "At X Movement, the development team had a daily check-in where we summed up how we were feeling in one word. To keep things interesting, I threw together a little angularJS app to help my team find more descriptive words. Later, it was built into an iOS app.",
     		"technologies": "",
-    		"imageUrl": "/img/projects/screenshots/.png"
+    		"imageUrl": "howAmI.png"
     	},
       {
     		"name": "Scal3d",
     		"subtitle": "Edtech Virtual Reality Experience",
-        "description": "",
+        "description": "An immersive, 3D visualization of the universe. Built in Unity and intended for use with an Oculus Rift, Scal3d is a sample of of useful, immersive and fun education technology. See a demonstration on the Scal3d website.",
     		"technologies": "",
-    		"imageUrl": "/img/projects/screenshots/.png"
+    		"imageUrl": "scal3d.png"
     	},
       {
     		"name": "Crowdlet",
     		"subtitle": "Event-Enhancing Wearable Technology",
-        "description": "",
+        "description": "An innovative wearable technology that enhances interactivity in live entertainment spaces using social media. It allows the user to control the colour of their wristband using twitter hashtags. Each hashtag corresponds to a specific colour, curated by the event’s coordinator. The bracelet was created using a web-enabled microcontroller, the Twitter API, an iOS app written in Objective-C, a 3D printed chassis and more.",
     		"technologies": "",
-    		"imageUrl": "/img/projects/screenshots/.png"
+    		"imageUrl": "crowdlet.jpg"
     	},
       {
     		"name": "Mashees",
     		"subtitle": "User-Generated Content Mash-Ups",
-        "description": "",
+        "description": "A fun tool that creates a mashup of gifs, audio and text. Content is driven by the users, so the amount of potential combinations is almost unlimited! Built in Node.js and React.",
     		"technologies": "",
-    		"imageUrl": "/img/projects/screenshots/.png"
+    		"imageUrl": "mashees.jpg"
     	}
     ]
 }
