c4d19b8911119193a407f3a940f5f3f4be5e1a88
Daniel Max <danielmaxsiegel@gmail.com>
2019-12-03 23:13:03 -0800

    Finished next version of the modals, dynamically add project image and technologies

--- js/projectModals.js
+++ js/projectModals.js
@@ -31,6 +31,26 @@ function getProjectTile(project, modalKey) {
 }
 
 function getProjectModal(project, modalKey) {
+  var projectImage = ''
+  var projectTechnologies = ''
+
+  if (project.imageUrl !== '') {
+    projectImage = '<img class="img-fluid rounded mb-2" src="img/projects/screenshots/' + project.imageUrl + '" alt="' + project.name + '">'
+  }
+
+  if (project.technologies !== []) {
+    var technologiesString = ''
+
+    for (var i = 0; i < project.technologies.length; ++i) {
+      technologiesString += project.technologies[i]
+      if (i < project.technologies.length - 1) {
+        technologiesString += " / "
+      }
+    }
+
+    projectTechnologies = '<div class="mb-2 technologies-font">' + technologiesString + '</div>'
+  }
+
   return '<div class="portfolio-modal modal fade" id="portfolioModal' + modalKey + '" tabindex="-1" role="dialog" aria-labelledby="portfolioModal' + modalKey + 'Label" aria-hidden="true">' +
     '<div class="modal-dialog modal-xl" role="document">' +
       '<div class="modal-content">' +
@@ -43,15 +63,12 @@ function getProjectModal(project, modalKey) {
           '<div class="container">' +
             '<div class="row justify-content-center">' +
               '<div class="col-lg-8">' +
-                '<h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">' + project.name + '</h2>' +
+                '<h3 class="portfolio-modal-title text-secondary text-uppercase mb-0 project-modal-title">' + project.name + '</h3>' +
                 '<div class="divider-custom">' +
                   '<div class="divider-custom-line"></div>' +
-                  '<div class="divider-custom-icon">' +
-                    '<i class="fas fa-star"></i>' +
-                  '</div>' +
-                  '<div class="divider-custom-line"></div>' +
                 '</div>' +
-                '<img class="img-fluid rounded mb-5" src="img/projects/screenshots/' + project.imageUrl + '" alt="' + project.name + '">' +
+                projectImage +
+                projectTechnologies +
                 '<p class="mb-5">' + project.description + '</p>' +
                 '<button class="btn btn-primary" href="#" data-dismiss="modal">' +
                   '<i class="fas fa-times fa-fw"></i>' +
@@ -72,78 +89,78 @@ function getProjects() {
     		"name": "5to9",
     		"subtitle": "Side Project Collective",
     		"description": "A collective for developers who love passion projects and collaboration. 5to9 is an effort by myself and some fellows from university to create a group where people can work on side projects without necessarily planning on monetizing them. The group has done work in NodeJS, Swift, Java, Python/Jupyter Notebook and more.",
-    		"technologies": "teamwork",
+    		"technologies": [],
     		"imageUrl": ""
     	},
-      {
-    		"name": "Crowdium",
-    		"subtitle": "Can you e-lance a whole business?",
-        "description": "",
-    		"technologies": "",
-    		"imageUrl": ""
-    	},
-      {
-    		"name": "Shelfaudit",
-    		"subtitle": "Author diversity analyzer",
-        "description": "",
-    		"technologies": "",
-    		"imageUrl": ""
-    	},
+      // {
+    	// 	"name": "Crowdium",
+    	// 	"subtitle": "Can you e-lance a whole business?",
+      //   "description": "",
+    	// 	"technologies": [],
+    	// 	"imageUrl": ""
+    	// },
+      // {
+    	// 	"name": "Shelfaudit",
+    	// 	"subtitle": "Author diversity analyzer",
+      //   "description": "",
+    	// 	"technologies": [],
+    	// 	"imageUrl": ""
+    	// },
       {
     		"name": "Quality Poetry",
     		"subtitle": "Randomly Generated Poetry",
         "description": "I was speaking with a professional photographer friend of mine about her difficulties when it comes to captioning her pictures. We joked about captioning everything with bad poetry, and Quality Poetry was born out of that conversation. Quality Poetry, found at qualitypoetry.com, is a webapp that procedurally generates poems.",
-    		"technologies": "",
+    		"technologies": ["TypeScript", "AngularJS", "NodeJS", "Twitter API", "Cron"],
     		"imageUrl": "qualityPoetry.png"
     	},
-      {
-    		"name": "LotOLife",
-    		"subtitle": "Procedural People Generator",
-        "description": "A tool that procedurally generates a fake human being using real-world statistics. Starting with gender and country of origin, the tool attempts to generate personal information, abilities, and more. The name comes from the fact that there’s a “Lot O’ Life”, and that the random selection of traits in life is kind of like playing the Lotto. Find Lotolife here.",
-    		"technologies": "",
-    		"imageUrl": "lotOLife.png"
-    	},
+      // {
+    	// 	"name": "LotOLife",
+    	// 	"subtitle": "Procedural People Generator",
+      //   "description": "A tool that procedurally generates a fake human being using real-world statistics. Starting with gender and country of origin, the tool attempts to generate personal information, abilities, and more. The name comes from the fact that there’s a “Lot O’ Life”, and that the random selection of traits in life is kind of like playing the Lotto. Find Lotolife here.",
+    	// 	"technologies": [],
+    	// 	"imageUrl": "lotOLife.png"
+    	// },
       {
     		"name": "Okay Pill",
     		"subtitle": "A Fashion Brand Website with a Twist",
         "description": "Okay pill is a clothing brand built around mental health awareness. As a friend of the founder, I volunteered to build their website per his designs. The website is built in NodeJS using React.",
-    		"technologies": "React",
+    		"technologies": ["ReactJS"],
     		"imageUrl": "okayPill.png"
     	},
       {
     		"name": "Music Visualizer",
     		"subtitle": "Unity Project",
         "description": "A simple audio visualizer built for fun in Unity, with scripts written in C#. The program detects the beat of music and pulses lights accordingly. It was meant to be an early prototype of a more fleshed-out music visualizer.",
-    		"technologies": "",
+    		"technologies": ["Unity", "C#"],
     		"imageUrl": "unityVisualizer.gif"
     	},
       {
     		"name": "HowAmI",
     		"subtitle": "Mood Tracking iOS App",
         "description": "At X Movement, the development team had a daily check-in where we summed up how we were feeling in one word. To keep things interesting, I threw together a little angularJS app to help my team find more descriptive words. Later, it was built into an iOS app.",
-    		"technologies": "",
+    		"technologies": ["Swift", "AngularJS", "PHP"],
     		"imageUrl": "howAmI.png"
     	},
-      {
-    		"name": "Scal3d",
-    		"subtitle": "Edtech Virtual Reality Experience",
-        "description": "An immersive, 3D visualization of the universe. Built in Unity and intended for use with an Oculus Rift, Scal3d is a sample of of useful, immersive and fun education technology. See a demonstration on the Scal3d website.",
-    		"technologies": "",
-    		"imageUrl": "scal3d.png"
-    	},
-      {
-    		"name": "Crowdlet",
-    		"subtitle": "Event-Enhancing Wearable Technology",
-        "description": "An innovative wearable technology that enhances interactivity in live entertainment spaces using social media. It allows the user to control the colour of their wristband using twitter hashtags. Each hashtag corresponds to a specific colour, curated by the event’s coordinator. The bracelet was created using a web-enabled microcontroller, the Twitter API, an iOS app written in Objective-C, a 3D printed chassis and more.",
-    		"technologies": "",
-    		"imageUrl": "crowdlet.jpg"
-    	},
-      {
-    		"name": "Mashees",
-    		"subtitle": "User-Generated Content Mash-Ups",
-        "description": "A fun tool that creates a mashup of gifs, audio and text. Content is driven by the users, so the amount of potential combinations is almost unlimited! Built in Node.js and React.",
-    		"technologies": "",
-    		"imageUrl": "mashees.jpg"
-    	}
+      // {
+    	// 	"name": "Scal3d",
+    	// 	"subtitle": "Edtech Virtual Reality Experience",
+      //   "description": "An immersive, 3D visualization of the universe. Built in Unity and intended for use with an Oculus Rift, Scal3d is a sample of of useful, immersive and fun education technology. See a demonstration on the Scal3d website.",
+    	// 	"technologies": ["Unity", "C#", "Oculus Rift"],
+    	// 	"imageUrl": "scal3d.png"
+    	// },
+      // {
+    	// 	"name": "Crowdlet",
+    	// 	"subtitle": "Event-Enhancing Wearable Technology",
+      //   "description": "An innovative wearable technology that enhances interactivity in live entertainment spaces using social media. It allows the user to control the colour of their wristband using twitter hashtags. Each hashtag corresponds to a specific colour, curated by the event’s coordinator. The bracelet was created using a web-enabled microcontroller, the Twitter API, an iOS app written in Objective-C, a 3D printed chassis and more.",
+    	// 	"technologies": [],
+    	// 	"imageUrl": "crowdlet.jpg"
+    	// },
+      // {
+    	// 	"name": "Mashees",
+    	// 	"subtitle": "User-Generated Content Mash-Ups",
+      //   "description": "A fun tool that creates a mashup of gifs, audio and text. Content is driven by the users, so the amount of potential combinations is almost unlimited! Built in Node.js and React.",
+    	// 	"technologies": [],
+    	// 	"imageUrl": "mashees.jpg"
+    	// }
     ]
 }
