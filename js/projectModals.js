92e4fa3dd8b24a324a2bbca28675f15a6c84d3e7
Daniel Max <danielmaxsiegel@gmail.com>
2019-12-08 13:12:11 -0800

    added ability to link to project

--- js/projectModals.js
+++ js/projectModals.js
@@ -33,6 +33,7 @@ function getProjectTile(project, modalKey) {
 function getProjectModal(project, modalKey) {
   var projectImage = ''
   var projectTechnologies = ''
+  var projectLink = ''
 
   if (project.imageUrl !== '') {
     projectImage = '<img class="img-fluid rounded mb-2" src="img/projects/screenshots/' + project.imageUrl + '" alt="' + project.name + '">'
@@ -49,6 +50,10 @@ function getProjectModal(project, modalKey) {
     }
 
     projectTechnologies = '<div class="mb-2 technologies-font">' + technologiesString + '</div>'
+  }
+
+  if (project.link !== '') {
+    projectLink = '<a href="' + project.link + '" rel="noopener noreferrer" target="_blank">See ' + project.name + ' here</a>'
   }
 
   return '<div class="portfolio-modal modal fade" id="portfolioModal' + modalKey + '" tabindex="-1" role="dialog" aria-labelledby="portfolioModal' + modalKey + 'Label" aria-hidden="true">' +
@@ -69,7 +74,8 @@ function getProjectModal(project, modalKey) {
                 '</div>' +
                 projectImage +
                 projectTechnologies +
-                '<p class="mb-5">' + project.description + '</p>' +
+                '<p class="mb-2">' + project.description + '</p>' +
+                '<div class="mb-5">' + projectLink + '</div>' +
                 '<button class="btn btn-primary" href="#" data-dismiss="modal">' +
                   '<i class="fas fa-times fa-fw"></i>' +
                   'Close Window' +
@@ -90,77 +96,88 @@ function getProjects() {
     		"subtitle": "Side Project Collective",
     		"description": "A collective for developers who love passion projects and collaboration. 5to9 is an effort by myself and some fellows from university to create a group where people can work on side projects without necessarily planning on monetizing them. The group has done work in NodeJS, Swift, Java, Python/Jupyter Notebook and more.",
     		"technologies": [],
-    		"imageUrl": ""
+    		"imageUrl": "",
+    		"link": "https://5to9.io"
     	},
       // {
     	// 	"name": "Crowdium",
     	// 	"subtitle": "Can you e-lance a whole business?",
-      //   "description": "",
+      //   "description": "While I was working at X Movement, I was feeling entrepreneurial but didn't have time to execute on anything (or an idea to execute on). ",
     	// 	"technologies": [],
-    	// 	"imageUrl": ""
+    	// 	"imageUrl": "",
+      // "link": ""
     	// },
       // {
     	// 	"name": "Shelfaudit",
     	// 	"subtitle": "Author diversity analyzer",
       //   "description": "",
     	// 	"technologies": [],
-    	// 	"imageUrl": ""
+    	// 	"imageUrl": "",
+      // "link": ""
     	// },
       {
     		"name": "Quality Poetry",
     		"subtitle": "Randomly Generated Poetry",
         "description": "I was speaking with a professional photographer friend of mine about her difficulties when it comes to captioning her pictures. We joked about captioning everything with bad poetry, and Quality Poetry was born out of that conversation. Quality Poetry, found at qualitypoetry.com, is a webapp that procedurally generates poems.",
-    		"technologies": ["TypeScript", "AngularJS", "NodeJS", "Twitter API", "Cron"],
-    		"imageUrl": "qualityPoetry.png"
+    		"technologies": ["TypeScript", "AngularJS", "NodeJS", "Twitter API", "Systemd"],
+    		"imageUrl": "qualityPoetry.png",
+    		"link": "https://twitter.com/QualityPoetry"
     	},
       // {
     	// 	"name": "LotOLife",
     	// 	"subtitle": "Procedural People Generator",
-      //   "description": "A tool that procedurally generates a fake human being using real-world statistics. Starting with gender and country of origin, the tool attempts to generate personal information, abilities, and more. The name comes from the fact that there’s a “Lot O’ Life”, and that the random selection of traits in life is kind of like playing the Lotto. Find Lotolife here.",
-    	// 	"technologies": [],
-    	// 	"imageUrl": "lotOLife.png"
+      //   "description": "A tool that procedurally generates a fake human being using real-world statistics. Starting with gender and country of origin, the tool attempts to generate personal information, abilities, and more. The name comes from the fact that there’s a “Lot O’ Life”, and that the random selection of traits in life is kind of like playing the Lotto.",
+    	// 	"technologies": ["PHP", "Bootstrap"],
+    	// 	"imageUrl": "lotOLife.png",
+      // "link": ""
     	// },
       {
     		"name": "Okay Pill",
     		"subtitle": "A Fashion Brand Website with a Twist",
-        "description": "Okay pill is a clothing brand built around mental health awareness. As a friend of the founder, I volunteered to build their website per his designs. The website is built in NodeJS using React.",
-    		"technologies": ["ReactJS"],
-    		"imageUrl": "okayPill.png"
+        "description": "Okay pill is a clothing brand built around mental health awareness whose original website I volunteered to build as a favour to the founder. The messaging on the site is utilitarian, telling you that everything will be okay once you take your pill; but when you enter your email, it twists to have messaging about the side effects and anxieties of mood-altering medications.",
+    		"technologies": ["NodeJS", "ReactJS"],
+    		"imageUrl": "okayPill.png",
+    		"link": ""
     	},
       {
     		"name": "Music Visualizer",
     		"subtitle": "Unity Project",
         "description": "A simple audio visualizer built for fun in Unity, with scripts written in C#. The program detects the beat of music and pulses lights accordingly. It was meant to be an early prototype of a more fleshed-out music visualizer.",
     		"technologies": ["Unity", "C#"],
-    		"imageUrl": "unityVisualizer.gif"
+    		"imageUrl": "unityVisualizer.gif",
+    		"link": ""
     	},
       {
     		"name": "HowAmI",
     		"subtitle": "Mood Tracking iOS App",
         "description": "At X Movement, the development team had a daily check-in where we summed up how we were feeling in one word. To keep things interesting, I threw together a little angularJS app to help my team find more descriptive words. Later, it was built into an iOS app.",
     		"technologies": ["Swift", "AngularJS", "PHP"],
-    		"imageUrl": "howAmI.png"
+    		"imageUrl": "howAmI.png",
+    		"link": ""
     	},
       // {
     	// 	"name": "Scal3d",
     	// 	"subtitle": "Edtech Virtual Reality Experience",
       //   "description": "An immersive, 3D visualization of the universe. Built in Unity and intended for use with an Oculus Rift, Scal3d is a sample of of useful, immersive and fun education technology. See a demonstration on the Scal3d website.",
     	// 	"technologies": ["Unity", "C#", "Oculus Rift"],
-    	// 	"imageUrl": "scal3d.png"
+    	// 	"imageUrl": "scal3d.png",
+      // "link": ""
     	// },
       // {
     	// 	"name": "Crowdlet",
     	// 	"subtitle": "Event-Enhancing Wearable Technology",
       //   "description": "An innovative wearable technology that enhances interactivity in live entertainment spaces using social media. It allows the user to control the colour of their wristband using twitter hashtags. Each hashtag corresponds to a specific colour, curated by the event’s coordinator. The bracelet was created using a web-enabled microcontroller, the Twitter API, an iOS app written in Objective-C, a 3D printed chassis and more.",
     	// 	"technologies": [],
-    	// 	"imageUrl": "crowdlet.jpg"
+    	// 	"imageUrl": "crowdlet.jpg",
+      // "link": ""
     	// },
       // {
     	// 	"name": "Mashees",
     	// 	"subtitle": "User-Generated Content Mash-Ups",
       //   "description": "A fun tool that creates a mashup of gifs, audio and text. Content is driven by the users, so the amount of potential combinations is almost unlimited! Built in Node.js and React.",
     	// 	"technologies": [],
-    	// 	"imageUrl": "mashees.jpg"
+    	// 	"imageUrl": "mashees.jpg",
+      // "link": ""
     	// }
     ]
 }
